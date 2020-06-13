6c8df0c7d703e0a8173874546998675bd6680377
Asia <kochanowska92@gmail.com>
2020-06-13 16:53:10 +0200

    Created functional conversion

--- src/components/CurrencyConventer.js
+++ src/components/CurrencyConventer.js
@@ -4,103 +4,101 @@ class CurrencyConventer extends React.Component {
     constructor() {
         super()
         this.state = {
-            amount: "",
-            from: "",
-            to: "",
+            amount: 1,
+            from: "GBP",
+            to: "USD",
+            rates: [],
+            currencies: [],
+            exchangeRate: ' '
         }
         this.handleChange = this.handleChange.bind(this)
     }
-    
+
+
+//Get list of currencies
+
+
+    componentDidMount() {
+        fetch("https://api.exchangeratesapi.io/latest")
+            .then(response => response.json())
+            .then(data => {
+              this.setState({
+                currencies: Object.keys(data['rates']).sort() 
+                })
+            })
+    }
+
+//get needed rates 
+    componentDidUpdate() {
+        fetch(`https://alt-exchange-rate.herokuapp.com/latest?base=${this.state.from}`)
+        .then(response => response.json())
+        .then(data => {
+            this.setState({
+              rates: data['rates'], 
+              exchangeRate: data.rates[this.state.to]
+            })
+        })
+    }
+
+
     handleChange(event) {
         const {name, value} = event.target
         this.setState({
-            [name]: value
+            [name]: value,
         }) 
     }
 
     render() {
+    const {currencies, from, to, amount, exchangeRate} = this.state;
+
+//display currencies 
+    const currencyChoice = currencies.map(currency =>
+        <option key={currency} value={currency}> {currency} </option>      
+      );
+
             return (
                 <div className="currency-box">
-                    <h3 className="heading">CurrencyConventer</h3>
-                    <form>
+                    <h3 className="heading">Currency Conventer</h3>
 
+                    <p className="currencyBox"> 1 {from} is { (1 * exchangeRate).toFixed([2]) } {to}</p>
+
+                    <form>
+<div className="currencyBox">
+
+<span className="title">Amount</span>
               <input 
                         name="amount" 
-                        value={this.state.amount} 
+                        type="number"
+                        value={amount} 
                         onChange={this.handleChange} 
                         placeholder="AMOUNT" 
                     />
+</div>
 
 
-                    <select 
-                        value={this.state.from} 
-                        name="from" 
-                        onChange={this.handleChange}
-                    >
-                        <option value="">-- FROM --</option>
-                        <option value="USD">USD (US$)</option>
-                        <option value="EUR">EUR (€)</option>
-                        <option value="JPY">JPY (¥)</option>
-                        <option value="GBP">GBP (£)</option>
-                        <option value="AUD">AUD (A$)</option>
-                        <option value="CAD">CAD (C$)</option>
-                        <option value="CHF">CHF (CHF)</option>
-                        <option value="CNY">CNY (元)</option>
-                        <option value="HKD">HKD (HK$)</option>
-                        <option value="NZD">NZD (NZ$)</option>
-                        <option value="SEK">SEK (kr)</option>
-                        <option value="KRW">KRW (₩)</option>
-                        <option value="SGD">SGD (S$)</option>
-                        <option value="NOK">NOK (kr)</option>
-                        <option value="MXD">MXN ($)</option>
-                        <option value="INR">INR (₹)</option>
-                        <option value="RUB">RUB (₽)</option>
-                        <option value="ZAR">ZAR (R)</option>
-                        <option value="TRY">TRY (₺)</option>
-                        <option value="BRL">BRL (R$)</option>
-                        <option value="TWD">TWD (NT$)</option>
-                        <option value="DKK">DKK (kr)</option>
-                        <option value="PLN">PLN (zł)</option>
-                    </select>
+<div className="currencyBox">
 
+<span className="title">From:</span>
+         <select  value={from} onChange={this.handleChange} name="from">
+            {currencyChoice}
+            <option>{from}</option>
+          </select>
 
-                    <select 
-                        value={this.state.to} 
-                        name="to" 
-                        onChange={this.handleChange}
-                    >
-                        <option value="">-- TO --</option>
-                        <option value="USD">USD (US$)</option>
-                        <option value="EUR">EUR (€)</option>
-                        <option value="JPY">JPY (¥)</option>
-                        <option value="GBP">GBP (£)</option>
-                        <option value="AUD">AUD (A$)</option>
-                        <option value="CAD">CAD (C$)</option>
-                        <option value="CHF">CHF (CHF)</option>
-                        <option value="CNY">CNY (元)</option>
-                        <option value="HKD">HKD (HK$)</option>
-                        <option value="NZD">NZD (NZ$)</option>
-                        <option value="SEK">SEK (kr)</option>
-                        <option value="KRW">KRW (₩)</option>
-                        <option value="SGD">SGD (S$)</option>
-                        <option value="NOK">NOK (kr)</option>
-                        <option value="MXD">MXN ($)</option>
-                        <option value="INR">INR (₹)</option>
-                        <option value="RUB">RUB (₽)</option>
-                        <option value="ZAR">ZAR (R)</option>
-                        <option value="TRY">TRY (₺)</option>
-                        <option value="BRL">BRL (R$)</option>
-                        <option value="TWD">TWD (NT$)</option>
-                        <option value="DKK">DKK (kr)</option>
-                        <option value="PLN">PLN (zł)</option>
-                    </select>
+<span className="title">To:</span>
+       <select value={to} onChange={this.handleChange} name="to">
+            {currencyChoice}
+            <option>{to}</option>
+          </select>
+
+</div>
 
             </form>
 
-                <p>Amount: {this.state.amount}</p>
-                <p>From: {this.state.from}</p>
-                <p>To: {this.state.to}</p>
+<div className="currencyBox">
 
+                <p>{amount} {from} is { (amount * exchangeRate).toFixed([2])} {to}</p> 
+
+</div>
 
                 </div>
             )
--- src/components/ExchangeRates.js
+++ src/components/ExchangeRates.js
@@ -1,11 +1,83 @@
 import React from "react"
 
-function ExchangeRates () {
+class ExchangeRates extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            amount: 1,
+            baseCurrency: "GBP",
+            rates: [],
+            currencies: []
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+
+    handleChange(event) {
+        const {name, value} = event.target
+        this.setState({
+            [name]: value,
+        }) 
+    }
+
+    //Get list of currencies
+    componentDidMount() {
+        fetch("https://api.exchangeratesapi.io/latest")
+            .then(response => response.json())
+            .then(data => {
+              this.setState({
+                currencies: Object.keys(data['rates']).sort() 
+                })
+            })
+    }
+
+    componentDidUpdate() {
+        fetch(`https://alt-exchange-rate.herokuapp.com/latest?base=${this.state.baseCurrency}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data['rates'])
+           // console.log(data.rates[this.state.baseCurrency])
+            this.setState({
+              rates: data['rates']
+            })
+        })
+    }
+
+
+render () {
+
+const {baseCurrency, currencies, rates } = this.state;
+
+//display currencies in a dropdown
+const currencyChoice = currencies.map(currency =>
+    <option key={currency} value={currency}> {currency} </option>      
+  );
+
+//display rates and currencies
+const currencyList = Object.entries(rates).map(([key,value])=>{
+    return (
+        <div>{key} : {value.toFixed([4])}</div>
+    );
+  })
+
 
 return (
-    <div>ExchangeRates</div>
-)
+    <div className="exchange-box">
+        <h3 className="heading">Exchange Rates</h3>
 
+        <span className="chooseBaseCurrency">Choose base currency</span>
+        <select  value={baseCurrency} onChange={this.handleChange} name="baseCurrency">
+            {currencyChoice}
+            <option>{baseCurrency}</option>
+          </select>
+
+<div className="currencyList">
+          <p>{ currencyList }</p>
+</div>
+        </div>
+)
+}
 }
 
 export default ExchangeRates
